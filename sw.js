3049c5c959a0813c8249ddef33a71329336e163d
Hussein Alwesi <Hussein.Al-wesi@iHorizons.com>
2025-07-30 18:44:39 +0300

    fixs

--- sw.js
+++ sw.js
@@ -1,9 +1,24 @@
 self.addEventListener('push', function (event) {
+    console.log('[Service Worker] Push received:', event);
+    if (!event.data) {
+        console.warn('[Service Worker] Push received but no data!');
+        return;
+    }
+
     const data = event.data.json();
+    console.log('[Service Worker] Push data:', data);
+
+
     event.waitUntil(
-        self.registration.showNotification(data.title, {
-            body: data.body,
-            icon: 'icon.png'
+        self.registration.showNotification('Test Notification', {
+            body: 'If you see this, your service worker works.',
         })
     );
+
+    // event.waitUntil(
+    //     self.registration.showNotification(data.title, {
+    //         body: data.body,
+    //         icon: 'icon.png'
+    //     })
+    // );
 });
